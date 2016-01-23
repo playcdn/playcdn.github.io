d07dc8e8196b3e7b58a75697547971281af29768
Chao Qun Yuan <Chao Qun Yuan>
2016-01-23 17:20:49 +0800

    fix iframe logout

---
+++ pkvn/162/Scripts/lang/th.js
@@ -0,0 +1,41 @@
+﻿(function (define) {
+	define(function () {
+		var lang = [];
+		lang["Account"] = "ข้อมูลบัญชี";
+		lang["Account Name"] = "ชื่อบัญชี";
+		lang["Account Number"] = "เลขบัญชี";
+		lang["Amount"] = "จำนวน";
+		lang["Balance"] = "ยอดดุล";
+		lang["Bank Name"] = "ชื่อธนาคาร";
+		lang["Com"] = "คอม";
+		lang["Confirm Password"] = "รหัสผ่านยืนยัน";
+		lang["Contact Number"] = "เบอร์ติดต่อกลับ";
+		lang["Currency"] = "สกุลเงิน";
+		lang["Date"] = "วัน";
+		lang["Deposit"] = "เงินฝาก";
+		lang["Description"] = "รายละเอียด";
+		lang["Detail"] = "รายละเอียด";
+		lang["E-mail"] = "อี-เมล";
+		lang["From Bank"] = "จากธนาคาร";
+		lang["Full Name"] = "ชื่อจริง";
+		lang["New Password"] = "รหัสผ่านใหม่";
+		lang["Old Password"] = "รหัสผ่านเก่า";
+		lang["Password"] = "รหัสผ่าน";
+		lang["Payment Method"] = "วิธีการฝากเงิน";
+		lang["Play Game"] = "การเล่นเกม";
+		lang["Referral"] = "รหัสผู้แนะนำ";
+		lang["Remark"] = "ช่วงเวลาของการฝากเงิน";
+		lang["Result"] = "ผล";
+		lang["Settled"] = "ทรงตัว";
+		lang["Stake"] = "เดิมพันสถานะ";
+		lang["Statement"] = "บัญชี";
+		lang["Status"] = "สถานะการฝากเงิน";
+		lang["To Bank"] = "ถึงธนาคาร";
+		lang["Transfer Date"] = "วันที่ฝาก";
+		lang["Username"] = "บัญชี";
+		lang["W/L"] = "สูญเสีย/ชนะ";
+		lang["Withdraw"] = "ถอนเงิน";
+		lang["Your Referral Code"] = "รหัสแนะนำของคุณ";
+		return lang;
+	})
+}(myGlobalRequire.define));
