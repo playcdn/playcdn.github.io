ab418ae4c41e85ffb744a7a837e552450ff16c3a
playcdn <playcdn@users.noreply.github.com>
2026-09-21 18:44:53 +0800

    gateway QR code correct level M

---
+++ pkvn/202/Scripts/view/account.js
@@ -0,0 +1,19 @@
+﻿(function (define) {
+define(["jquery", "api", "translate"], function ($, api, translate) {
+		return function () {
+			api.POST('/api/getAccInfo', "", accountSuccess, accountFail);
+		}
+
+		function accountSuccess(result, errCode, errText) {
+			var json = $.parseJSON(result);
+			$('#txtAccUsername').html(json.username);
+			$('#txtAccCurrency').html(json.currency);
+			$('#txtAccFullname').html(json.fullname);
+			$('#txtAccContact').html(json.contact);
+			$('#txtAccEmail').html(json.email);
+		}
+		function accountFail(errCode, errText) {
+			console.log(errCode + ":" + errText);
+		}
+})
+}(myGlobalRequire.define));
